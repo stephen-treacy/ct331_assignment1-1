4368a74e105fdab72835ef5c695962044d9ebda6
stephen-treacy <stephenttreacy@gmail.com>
2017-10-11 20:38:40 +0100

    Update assignment.c
    They're all the same size. Pointers are the same size as the integer they're pointing to.

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -5,7 +5,7 @@
 		printf("Hello assignment1.\n");
 		printf("Size of int: %ld\n", sizeof(int));
 		printf("Size of int pointer: %ld\n", sizeof(int*));
-		printf("Size of long number: %ld\n", sizeof(long));
+		printf("Size of long: %ld\n", sizeof(long));
 		printf("Size of double pointer: %ld\n", sizeof(double*));
 		printf("Size of char pointer pointer: %ld\n", sizeof(char**));
 		return 0;
