b02897d70f5473a7251e782500feacf813e31bd0
stephen-treacy <stephenttreacy@gmail.com>
2017-10-11 17:50:30 +0100

    Update assignment.c

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,12 @@
+#include "stdafx.h"
 #include <stdio.h>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
-}
+	int main(int arg, char* argc[]) {
+		printf("Hello assignment1.\n");
+		printf("Size of int: %ld\n", sizeof(int));
+		printf("Size of int pointer: %ld\n", sizeof(int*));
+		printf("Size of long number: %ld\n", sizeof(long));
+		printf("Size of double pointer: %ld\n", sizeof(double*));
+		printf("Size of char pointer pointer: %ld\n", sizeof(char**));
+		return 0;
+	}
